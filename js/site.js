fb7660530bfdf1e0523034a1477b39bbc029c211
StudiedQuill <jmaravillalemus@hawk.iit.edu>
2024-02-22 17:23:00 -0600

    For small devices
    Deleting a lot of the checkbox stuff and leaving it for later. Focusing on styling webpage for small devices right now.

--- js/site.js
+++ js/site.js
@@ -2,9 +2,5 @@
 jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
-  // From week 6 demo 12 site.js
-  $("#alt-box").on("click", function () {
-    $("#age-verification").prop("checked", true);
-    $("#alt-box").html("<p>&#10004;</p>");
   });
 });
