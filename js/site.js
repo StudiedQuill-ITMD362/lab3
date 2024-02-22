ac8eec0456df749d83218bad8e1d22ef8f7f0d2b
StudiedQuill <jmaravillalemus@hawk.iit.edu>
2024-02-22 15:36:47 -0600

    added css, js. and changed things in html
    Added css to change background color, h1 color and changed the placement of it. Also added css for the different checkmark. Added js for the checkmark when clicked. Changed the placement of the js script in the html file to the top as well as added a div on top of the checkmark.

--- js/site.js
+++ js/site.js
@@ -2,4 +2,9 @@
 jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
+  // From week 6 demo 12 site.js
+  $("#alt-box").on("click", function () {
+    $("#age-verification").prop("checked", true);
+    $("#alt-box").html("<p>&#10004;</p>");
+  });
 });
